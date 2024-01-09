477069f6b299d2c97a5bf64d85bea6158f417410
Kadek Sudiartha <sudieartha@gmail.com>
2024-01-09 11:29:09 +0800

    re-initial project

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { config } from "dotenv";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
-import { readFileSync } from "fs";
 
 config();
 
@@ -11,45 +10,69 @@ const main = async () => {
 
   try {
     const sdk = ThirdwebSDK.fromPrivateKey(
-      process.env.WALLET_PRIVATE_KEY,
-      "mumbai",
+      process.env.WALLET_PRIVATE_KEY ?? "",
+      "polygon",
       {
         secretKey: process.env.THIRDWEB_SECRET_KEY,
       }
     );
 
-    const contractAddress = await sdk.deployer.deployNFTDrop({
-      name: "My Drop",
-      primary_sale_recipient: "0x39Ab29fAfb5ad19e96CFB1E1c492083492DB89d4",
-    });
+    const contract = await sdk.getContract(process.env.CONTRACT_ADDRESS ?? "");
+    const address = process.env.WALLET_ADDRESS ?? ""; // address of the wallet you want to claim the NFTs
 
-    console.log("Contract address: ", contractAddress);
+    const metadata = async () => {
+      const metadata = await contract.metadata.get();
+      console.log(metadata);
+    }
 
-    const contract = await sdk.getContract(contractAddress, "nft-drop");
+    const nfts = async () => {
+      const nfts = await contract.erc1155.getAll();
+      console.log(nfts);
+    }
 
-    const metadatas = [
-      {
-        name: "Blue Star",
-        description: "A blue star NFT",
-        image: readFileSync("assets/blue-star.png"),
-      },
-      {
-        name: "Red Star",
-        description: "A red star NFT",
-        image: readFileSync("assets/red-star.png"),
-      },
-      {
-        name: "Yellow Star",
-        description: "A yellow star NFT",
-        image: readFileSync("assets/yellow-star.png"),
-      },
-    ];
+    const claimConditions = async (total: number, amount: number) => {
+      const presaleStartTime = new Date();
+      const claimConditions = [
+        {
+          startTime: presaleStartTime, // start the presale now
+          maxClaimableSupply: amount, // limit how many mints for this presale
+          price: 0, // claim price
+          address: address // address of the currency to accept && limit minting to only certain addresses
+        }
+      ];
+      
+      for (let i = total; i >= 0; i--) {
+        try {
+          await contract.erc1155.claimConditions.set(i, claimConditions);
+          const tx = await contract.erc1155.claimTo(address, i, amount);
+          const receipt = tx.receipt; // the transaction receipt
+          console.log(receipt);
+          console.log("Success Mint " + i);
+        } catch (e) {
+          console.error("Something went wrong: ", e);
+        }
+      }
+    }
 
-    await contract.createBatch(metadatas);
-    console.log("Created batch successfully!");
+    const burn = async (total: number, amount: number) => {
+      try {
+        for (let i = 0; i < total; i++) {
+            const result = await contract.erc1155.burn(i, amount);
+            console.log(result);
+            console.log("Success Burn " + i);
+        }
+      } catch (e) {
+        console.error("Something went wrong: ", e);
+      }
+    }
+    
+    // metadata(); // get the metadata of the contract
+    claimConditions(9734, 500); // 
+    // burn(9999, 1); // burn all the NFTs
+
   } catch (e) {
     console.error("Something went wrong: ", e);
   }
-};
+}
 
 main();
